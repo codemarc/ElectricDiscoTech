455f9b17ab8d5ec35b9fce41b742c9b9c337ef76
Marc Greenberg <codemarc@gmail.com>
2015-11-23 18:24:39 -0500

    reorganized and refactored to make more logical sense

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['less/**/*.less', 'ts/**/*.ts'], // which files to watch
+                tasks: ['less', 'ts', 'uglify'],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
+
         less: {
             development: {
                 options: {
@@ -16,57 +29,45 @@ module.exports = function(grunt) {
             }
         },
 
-        watch: {
-            options: {
-                livereload: false,
-            },
-            styles: {
-                files: ['less/**/*.less','ts/**/*.less','ts/**/*.ts'], // which files to watch
-                tasks: ['less','ts','uglify'],
+        ts: {
+            default: {
+                outDir: "./js",
+                src: ["**/*.ts", "typings/**/*.ts", "!node_modules/**/*.ts", "!bower_components/**/*.*"],
                 options: {
-                    nospawn: true
+                    verbose: true,
+                    sourceMap: false
                 }
             }
         },
         
-         ts: {
-             default : {
-                outDir: "./js",
-                src: ["**/*.ts","typings/**/*.ts","!node_modules/**/*.ts","!bower_components/**/*.*"], 
-                options: {
-                    verbose: true,
-                    sourceMap: false
-                }                
-            }
-            
-        },
-        
         uglify: {
-		  options: {
-			preserveComments: false
-		  },
-          main: {
-            files: {
-                '../js/login.min.js': ['js/login.js'],
-                '../js/index.min.js': ['js/index.js'],
-                '../js/test.min.js': 
-                [   
-                    'js/test/testobj.js'
-                    , 'js/test/testobjs.js'
-                    , 'js/test/tests.js'
-                    , 'js/test/models.js'
-                    , 'js/test/views.js'
-                    , 'js/test/actions.js'
-                ]
+            options: {
+                preserveComments: false
+            },
+            main: {
+                files: {
+                    '../js/edt.min.js': ['js/edt.js']
+                    // '../js/login.min.js': ['js/login.js'],
+                    // '../js/index.min.js': ['js/index.js'],
+                    // '../js/test.min.js':
+                    // [
+                    //     'js/test/testobj.js'
+                    //     , 'js/test/testobjs.js'
+                    //     , 'js/test/tests.js'
+                    //     , 'js/test/models.js'
+                    //     , 'js/test/views.js'
+                    //     , 'js/test/actions.js'
+                    // ]
+                }
             }
-          }
-   	   }
+        }
             
     });
 
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    
     grunt.registerTask('default', ['less', 'watch', 'ts']);
 };
